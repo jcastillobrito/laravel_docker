6fba4cb5b6ca7032e92617f46113177b73b88bc6
Juan Carlos Castillo Brito <jccastillo.brito@gmail.com>
2023-03-12 18:01:24 -0300

    fix composer

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,9 @@ FROM php:8.1-fpm
 RUN apt-get update && apt-get install -y \
     git \
     zip \
-    unzip
+    unzip 
+
+RUN docker-php-ext-install pdo_mysql 
 
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
--- src/database/migrations/2023_03_12_194904_create_categorias_table.php
+++ src/database/migrations/2023_03_12_194904_create_categorias_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('categorias', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->string('icono');
             $table->timestamps();
         });
     }
